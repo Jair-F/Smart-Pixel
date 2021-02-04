0a5ae0229c96c3a4f853100affca7f93e27bbda0
Jair <githubg9@gmail.com>
2021-02-04 20:26:41 +0200

    Relay-Klassse hinzugefuegt

---
+++ lib/Relay.hpp
@@ -0,0 +1,33 @@
+#ifndef _RELAY_HPP_INCLUDED_
+#define _RELAY_HPP_INCLUDED_
+
+class Relay{
+public:
+	Relay() { }
+	Relay(unsigned short _pin, bool _status = false);
+	Relay(const Relay&) = default;
+	Relay& operator=(Relay&) = default;
+	~Relay() { }
+
+	void activate()		{ digitalWrite(pin, HIGH);	}
+	void deactivate()	{ digitalWrite(pin, LOW);	}
+	bool status()		{ return digitalRead(pin);	}
+
+	void setPin(unsigned short _pin);
+	unsigned short int getPin() { return pin; }
+private:
+	unsigned short int pin;
+};
+
+// Implementationen
+
+Relay::Relay(unsigned short _pin, bool _status): pin(_pin) {
+	pinMode(_pin, OUTPUT);
+	digitalWrite(pin, _status);
+}
+
+void Relay::setPin(unsigned short _pin) {
+	pin = _pin;
+	pinMode(pin, OUTPUT);
+}
+#endif // _RELAY_HPP_INCLUDED_
