2b5bb9ba9b8c042ee312d347ac342e5de0af3625
Jair <Github-sketch-code@users.noreply.github.com>
2021-02-25 17:44:30 +0200

    Akt. auf sen. von Daten in extra Funktion ausgel.

--- lib/WiFiUtils.hpp
+++ lib/WiFiUtils.hpp
@@ -11,17 +11,17 @@
 #ifndef _WIFIUTILS_HPP_INCLUDED_
 #define _WIFIUTILS_HPP_INCLUDED_
 
-/**
- * @param stat WiFi-Status
- * @return Textbasierte Fehlermeldung von WiFi-Status "stat"
- */
-String wifiStatusUserOutput(wl_status_t stat);
-
 void initWifi();
 
 void initDNS();
 
 void initWebServer();
+
+/**
+ * @param stat WiFi-Status
+ * @return Textbasierte Fehlermeldung von WiFi-Status "stat"
+ */
+String wifiStatusUserOutput(wl_status_t stat);
 
 /**
  * @param filename Pfad zu Datei
@@ -30,15 +30,65 @@ void initWebServer();
 String getFileType(String filename);
 
 /**
- * Sendet dem Client via HTTP-Webserver die geforderte Datei
- * @param path Pfad zur Datei, die vom Client angefordert wurde
- * @return true wenn es die Datei gibt und kein fehler beim oeffnen gibt - sonst false
+ * Schickt die vom Cliente gefordereten Dateien, gibt Fehlermeldungen bei nicht gefundenen Dateien und
+ * fuehrt Aktionen auf zurueckgesendete Daten aus.
  */
-bool handleFromClientRequestedFile(String path);
+void handleClientandActions();
+
+/**
+ * @return leerer String wenn es die Datei nicht gibt, sonst Pfad zur Datei
+ * @param path Pfad-URL zur angeforderten Datei
+ */
+String find_path_to_req_File(String path);
 
 
 // --------------------------- Implementationen ---------------------------
 
+// https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
+void initWifi() {
+	if(WiFiAccessPointMode == true) {
+		bool WiFiAccessPointCreated = WiFi.softAP(WiFiName, WiFiPassword, 1, 0, MaxWiFiCon);
+		if(! WiFiAccessPointCreated) {
+			throw WiFi_Exception("WiFi AccessPoint failed to start!");
+		}
+	} else {
+		Serial.println("Networks in range: ");
+		int8_t numOfDiscoverdNetworks = WiFi.scanNetworks();
+		for(int8_t i = 0; i < numOfDiscoverdNetworks; i++) {
+			Serial.println(String(WiFi.SSID(i) + ' ' + WiFi.encryptionType(i)));
+		}
+		WiFi.scanDelete(); // Gefundene Netzwerke entfernen, im fall dass wenn nochmal gesucht wird, sie nicht erscheinen
+		
+		wl_status_t wifiStatus = WiFi.begin(WiFiName, WiFiPassword);
+
+		while(wifiStatus != WL_CONNECTED) {
+			WiFi.waitForConnectResult();	// Warten, bis WiFi ein Ergebnis hat(Timout,Successfully connected...)
+			wifiStatus = WiFi.status();
+
+			Serial.println(wifiStatusUserOutput(wifiStatus));
+			delay(1000);
+		}
+	}
+}
+
+// https://tttapa.github.io/ESP8266/Chap08%20-%20mDNS.html
+void initDNS() {
+	if(MDNS.begin(Hostname, WiFi.localIP())) {
+
+	} else {
+		throw std::runtime_error("DNS-Service failed to start!");
+	}
+}
+
+// https://tttapa.github.io/ESP8266/Chap10%20-%20Simple%20Web%20Server.html
+// https://arduino.stackexchange.com/questions/39599/esp8266-cannot-read-post-parameters
+
+//??
+// https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/server-examples.html
+void initWebServer() {
+	webserver.onNotFound(handleClientandActions);
+	webserver.begin();
+}
 
 // https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html#check-return-codes
 String wifiStatusUserOutput(wl_status_t stat) {
@@ -85,39 +135,60 @@ String wifiStatusUserOutput(wl_status_t stat) {
 	return message;
 }
 
-// https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
-void initWifi() {
-	if(WiFiAccessPointMode == true) {
-		bool WiFiAccessPointCreated = WiFi.softAP(WiFiName, WiFiPassword, 1, 0, MaxWiFiCon);
-		if(! WiFiAccessPointCreated) {
-			throw WiFi_Exception("WiFi AccessPoint failed to start!");
-		}
+
+void handleClientandActions() {
+	// Datei-streamen
+	String pathToFile = find_path_to_req_File(webserver.uri());
+	if(pathToFile.isEmpty()) {	// Wenn es den vom Client verlangten Pfad nicht gibt eine Fehlermeldung schicken
+		webserver.send(404, "text/plain", "404: Site not found");
 	} else {
-		Serial.println("Networks in range: ");
-		int8_t numOfDiscoverdNetworks = WiFi.scanNetworks();
-		for(int8_t i = 0; i < numOfDiscoverdNetworks; i++) {
-			Serial.println(String(WiFi.SSID(i) + ' ' + WiFi.encryptionType(i)));
+		File file = SPIFFS.open(pathToFile, "r");
+		if(! file) {
+			Serial.println(String("Fehler beim oeffnen der Datei" + pathToFile));
+			webserver.send(404, "text/plain", String("Fehler beim oeffnen der Datei" + pathToFile));
+		} else {
+			webserver.streamFile(file, getFileType(pathToFile));
 		}
-		WiFi.scanDelete(); // Gefundene Netzwerke entfernen, im fall dass wenn nochmal gesucht wird, sie nicht erscheinen
-		
-		wl_status_t wifiStatus = WiFi.begin(WiFiName, WiFiPassword);
+		file.close();
+	}
 
-		while(wifiStatus != WL_CONNECTED) {
-			WiFi.waitForConnectResult();	// Warten, bis WiFi ein Ergebnis hat(Timout,Successfully connected...)
-			wifiStatus = WiFi.status();
-
-			Serial.println(wifiStatusUserOutput(wifiStatus));
-			delay(1000);
+	// Aktionen auf zurueckgesendete Daten
+	if(webserver.method() == HTTP_POST) {
+		Serial.println("Params:");
+		Serial.println(webserver.arg("Color"));
+		for(unsigned short i = 0; i < webserver.args(); i++) {
+			if(webserver.argName(i) == "Color") {
+				for(unsigned short pixel = 0; pixel < RGB_LEDS.numPixels(); pixel++) {
+					RGB_LEDS.setPixelColor(pixel, RGBHexToColor(webserver.arg(i).c_str()));
+				}
+				RGB_LEDS.show();
+			}
+			/**
+			 * "plain" gibt nochmals die Argumente als ein String zurueck wie sie oben im Browser in der Titelleiste stehen wuerde,
+			 * wenn im html "method=GET" steht.
+			 */
+			else if(webserver.argName(i) == "plain") {
+				continue;
+			} else {
+				Serial.println("Unknow Arg:");
+				Serial.print(webserver.argName(i));
+				Serial.print(':');
+				Serial.println(webserver.arg(i));
+			}
 		}
 	}
 }
 
-// https://tttapa.github.io/ESP8266/Chap08%20-%20mDNS.html
-void initDNS() {
-	if(MDNS.begin(Hostname, WiFi.localIP())) {
-
+// https://tttapa.github.io/ESP8266/Chap11%20-%20SPIFFS.html
+String find_path_to_req_File(String path) {
+	
+	if(path.endsWith("/")) { path += "index.html"; } // Wenn ein Ordner verlangt wird, die index.html Datei geben
+	String FileType = getFileType(path);
+	if(SPIFFS.exists(path)) {
+		return path;
 	} else {
-		throw std::runtime_error("DNS-Service failed to start!");
+		path.clear();
+		return path;
 	}
 }
 
@@ -131,70 +202,6 @@ String getFileType(String filename) {
 	else if (filename.endsWith(".jpeg"))	return "image/jpeg";
 	else if (filename.endsWith("jpg"))		return "image/jpeg";
 	else									return "text/plain";
-}
-
-// https://tttapa.github.io/ESP8266/Chap10%20-%20Simple%20Web%20Server.html
-// https://arduino.stackexchange.com/questions/39599/esp8266-cannot-read-post-parameters
-
-//??
-// https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/server-examples.html
-void initWebServer() {
-	webserver.onNotFound (
-		[] () {		// inline Funktion - muss nicht extra globale Funktion machen
-			bool pathExist = handleFromClientRequestedFile(webserver.uri());
-			if(! pathExist) {	// Wenn es den vom Client verlangten Pfad nicht gibt eine Fehlermeldung schicken
-				webserver.send(404, "text/plain", "404: Site not found");
-			} else {
-				if(webserver.method() == HTTP_POST) {
-					Serial.println("Params:");
-					Serial.println(webserver.arg("Color"));
-					for(unsigned short i = 0; i < webserver.args(); i++) {
-						if(webserver.argName(i) == "Color") {
-							for(unsigned short pixel = 0; pixel < RGB_LEDS.numPixels(); pixel++) {
-								RGB_LEDS.setPixelColor(pixel, RGBHexToColor(webserver.arg(i).c_str()));
-							}
-							RGB_LEDS.show();
-						}
-						/**
-						 * "plain" gibt nochmals die Argumente als ein String zurueck wie sie oben im Browser in der Titelleiste stehen wuerde,
-						 * wenn im html "method=GET" steht.
-						 */
-						else if(webserver.argName(i) == "plain") {
-							continue;
-						} else {
-							Serial.println("Unknow Arg:");
-							Serial.print(webserver.argName(i));
-							Serial.print(':');
-							Serial.println(webserver.arg(i));
-						}
-					}
-				}
-			}
-		}
-	);
-	webserver.begin();
-}
-
-// https://tttapa.github.io/ESP8266/Chap11%20-%20SPIFFS.html
-bool handleFromClientRequestedFile(String path) {
-
-	Serial.print("Angefordert: ");
-	Serial.print(path);
-
-	if(path.endsWith("/")) { path += "index.html"; } // Wenn ein Ordner verlangt wird, die index.html Datei geben
-	String FileType = getFileType(path);
-	if(SPIFFS.exists(path)) {
-		File file = SPIFFS.open(path, "r");
-		if(! file) {
-			Serial.println("Fehler beim oeffnen der Datei");
-			return false;
-		}
-		webserver.streamFile(file, FileType);
-		file.close();
-		Serial.print(" -> ");
-		Serial.println(path + " gesendet");
-	}
-	return true;
 }
 
 #endif // _WIFIUTILS_HPP_INCLUDED_
