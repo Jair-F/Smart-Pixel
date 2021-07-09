a10a15cb45e039eb190954c51bf0a1a4bbf02874
Jair <jair.fehlauer@gmail.com>
2021-07-09 20:44:55 +0300

    Added global Constants

---
+++ lib/GlobalConstants.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+
+
+// Config-Keywords
+#define WIFI_NAME				"WiFiName"
+#define WIFI_PASSWORD			"WiFiPassword"
+#define MAX_CONNECTIONS			"MaxConnections"
+#define WIFI_ACCESSPOINT_MODE	"WiFiAccessPointMode"
+#define HOSTNAME				"Hostname"
+
+// Websocket Acions
+#define RGB_COLOR					"RGB-Color"
+#define EFFECT						"Effect"
+#define EFFECT_RUNNING				"EffektRunning"
+#define EFFECT_SPEED				"EffectSpeed"
+#define HUMIDITY					"Humidity"
+#define TEMPERATURE					"Temperature"
+#define RELAY_STATUS				"RelayStatus"
+#define RELAY_NAME					"RelayName"
+#define WRITE_CONFIG				"write-config"
+#define PIR_SENSOR_ACTIVE_REPORT	"PirSensorActiveReport"
+// Reboot the board
+#define REBOOT						"reboot"
+// Send Command to client to show him a message(pop up window)
+#define CLIENT_ALERT				"ClientAlert"
+#define NUM_OF_CONNECTED_CLIENTS	"NumOfConnectedClients"
+
+// conifg-Groups
+#define WIFI	"WiFi"
+#define SERVER	"Server"
